4a85cc17a8185b0fbcda16984165c3da872fdf24
xxx <xxxx@qq.com>
2021-09-22 17:47:26 +0800

    all

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2021-09-15 10:59:06
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-09-22 17:45:50
+-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="container">
+    <Search></Search>
+    <List></List>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Search from './components/Search.vue'
+import List  from './components/List.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Search,List
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2021-09-22 17:37:06
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-09-22 17:44:38
+-->
+<template>
+  <section class="jumbotron">
+  <h3 class="jumbotron-heading">Search Github Users</h3>
+  <div>
+    <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+    name:'Search'
+}
+</script>
+
