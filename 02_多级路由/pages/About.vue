15f95457d47e5bca5096cba06b8d664ffe8a764c
td_code <1098691502@qq.com>
2021-09-25 19:17:40 +0800

    1

---
+++ 02_多级路由/pages/About.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2021-09-25 13:49:34
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-09-25 13:50:58
+-->
+<template>
+      <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default {
+    name:'About'
+}
+</script>
+
+<style>
+
+</style>
---
+++ 02_多级路由/pages/News.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2021-09-25 18:55:49
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-09-25 18:57:03
+-->
+<template>
+   <ul>
+   <li>news001</li>
+   <li>news002</li>
+   <li>news003</li>
+ </ul>
+</template>
+
+<script>
+export default {
+    name:'News',
+}
+</script>
+
+<style>
+
+</style>
